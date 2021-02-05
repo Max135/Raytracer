118309efc85c45b51417b4757e4f93faa3a9f0f4
Max <maxlamenace_2000@hotmail.com>
2021-02-04 20:09:37 -0500

    Main uses threads

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <ctime>
 #include <cmath>
+#include <thread>
 #include "raytracer/features/Tuple/Tuples.h"
 #include "raytracer/features/Canvas/Canvas.h"
 #include "raytracer/features/Matrix/Matrices.h"
@@ -27,6 +28,9 @@ void projectileTrajectory();
 
 void traceSphere();
 
+void writePixel(std::vector<Intersection> xs, Ray *ray, Canvas *canvas, Light *light, int x, int y);
+void calculateColisions(int canvasSize, float half, float pixelSize, float worldY, float wallZ, const Point& rayOrigin, Sphere *sphere, Canvas *canvas, Light *light, int y);
+
 
 int main() {
     std::clock_t startTime;
@@ -48,15 +52,14 @@ int main() {
 //http://boostorg.github.io/compute/
 
 void traceSphere() {
-    const int canvasSize = 300;
+    const int canvasSize = 5000;
     const int wallSize = 7;
     const int wallZ = 10;
 
     Sphere sphere;
-    sphere.material.color = Color(1, 0.2, 1);
-//    sphere.transform.shear(1, 0, 0.01, 0, 1, 0).scale(0.5, 1, 1);
+    sphere.material.color = Color(0, 0, 1);
+    sphere.transform.shear(1, 0, 0.01, 0, 1, 0).scale(0.5, 1, 1);
 
-    Color color(0, 0, 255);
     Canvas canvas(canvasSize, canvasSize);
 
     Light light(Point(-10, 10, -10), Color(1, 1, 1));
@@ -66,26 +69,46 @@ void traceSphere() {
     float pixelSize = (float) wallSize / canvasSize;
     float half = wallSize / 2.0;
 
-    for (int y = 0; y < canvasSize - 1; ++y) {
-        float worldY = half - pixelSize * (float) y;
-        for (int x = 0; x < canvasSize - 1; ++x) {
-            float worldX = -half + pixelSize * (float) x;
-            Point position(worldX, worldY, wallZ);
-            Ray ray(rayOrigin, (position - rayOrigin).normalize());
-            std::vector<Intersection> xs = ray.intersect(&sphere);
+    for (int y = 0; y < canvasSize - 1; y+=10) {
+        if (y % 100 == 0)
+            std::cout << y << std::endl;
 
-            if (!xs.empty()) {
-                Intersection hit = xs[0];
-                Tuple point = ray.position(hit.t);
-                Tuple normal = hit.sphere->normalAt(point);
-                Tuple eye = -(ray.direction);
-
-                canvas.writePixel(x, y, hit.sphere->material.lighting(light, point, eye, normal));
-            }
+        std::vector<std::thread> threads;
+        for (int limit = y; limit < y + 10; limit++) {
+            float worldY = half - pixelSize * (float) y;
+            std::thread thread(calculateColisions, canvasSize, half, pixelSize, worldY, wallZ, rayOrigin, &sphere,
+                               &canvas, &light, limit);
+            threads.push_back(std::move(thread));
+//          calculateColisions(canvasSize, half, pixelSize, worldY, wallZ, rayOrigin, &sphere, &canvas, &light, y);
+        }
+        for(auto & thread : threads) {
+            thread.join();
         }
     }
 
     canvas.save();
+}
+
+void calculateColisions(int canvasSize, float half, float pixelSize, float worldY, float wallZ, const Point& rayOrigin, Sphere *sphere, Canvas *canvas, Light *light, int y) {
+    for (int x = 0; x < canvasSize - 1; ++x) {
+        float worldX = -half + pixelSize * (float) x;
+        Point position(worldX, worldY, wallZ);
+        Ray ray(rayOrigin, (position - rayOrigin).normalize());
+        std::vector<Intersection> xs = ray.intersect(sphere);
+
+        if (!xs.empty()) {
+            writePixel(xs, &ray, canvas, light, x, y);
+        }
+    }
+}
+
+void writePixel(std::vector<Intersection> xs, Ray *ray, Canvas *canvas, Light *light, int x, int y) {
+    Intersection hit = xs[0];
+    Tuple point = ray->position(hit.t);
+    Tuple normal = hit.sphere->normalAt(point);
+    Tuple eye = -(ray->direction);
+
+    canvas->writePixel(x, y, hit.sphere->material.lighting(*light, point, eye, normal));
 }
 
 void projectileTrajectory() {
