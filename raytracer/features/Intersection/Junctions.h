04ef7b0c78d24ca3efb6081d385ebbec079279db
Max <maxlamenace_2000@hotmail.com>
2021-02-06 11:45:22 -0500

    Forgot to add .h file to commit

---
+++ raytracer/features/Intersection/Junctions.h
@@ -0,0 +1,11 @@
+//
+// Created by Max on 2021-02-06.
+//
+
+#ifndef RAYTRACER_JUNCTIONS_H
+#define RAYTRACER_JUNCTIONS_H
+
+#include "Intersection.h"
+#include "Intersections.h"
+
+#endif //RAYTRACER_JUNCTIONS_H
